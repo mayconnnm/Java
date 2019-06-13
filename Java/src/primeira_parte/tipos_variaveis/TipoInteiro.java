63dea37af2c25c2875360edddffed7a97fd6936d
Maycon <mayconnmorais@gmail.com>
2019-06-13 20:38:29 -0300

    Primeiros Passos

---
+++ Java/src/primeira_parte/tipos_variaveis/TipoInteiro.java
@@ -0,0 +1,18 @@
+package primeira_parte.tipos_variaveis;
+
+public class TipoInteiro {
+
+	public static void main(String[] args) {
+
+		System.out.println("Idade");
+
+		int idade;
+		idade = 37;
+		System.out.println(idade);
+		idade = 30 + 30;
+		System.out.println(idade);
+		idade = (7 * 5) + 2;
+		System.out.println("a idade é: " + idade);
+
+	}
+}
